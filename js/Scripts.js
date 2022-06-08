533e224c3061270fc6ccaca5a03619fca38c6be5
Emmanuel341976 <Emmanuel341976@users.noreply.github.com>
2022-06-08 15:46:01 +0200

    Fin TP Bootstrap

--- js/Scripts.js
+++ js/Scripts.js
@@ -14,3 +14,42 @@ function openNav() {
 function closeNav() {
   sidenav.classList.remove("active");
 }
+
+
+function update() {
+  
+  const elem1 = document.getElementById("stylistes");
+  const rect1 = elem1.getBoundingClientRect();
+  const elem2 = document.getElementById("extraordinaire");
+  const rect2 = elem2.getBoundingClientRect();
+  const elem3 = document.getElementById("projet");
+  const rect3 = elem3.getBoundingClientRect();
+
+  let toprect1 = rect1.top;
+  let toprect2 = rect2.top;
+  let toprect3 = rect3.top;
+  let hauteur = window.innerHeight;
+
+  if (toprect1 >= 0 && toprect1 < hauteur)
+    {
+      document.getElementById("verticaltext").innerHTML = "OBTENEZ DES CONSEILS GRATUITS SUR MESURE";
+    }
+
+  if (toprect2 >= 0 && toprect2 < hauteur)
+  {
+    document.getElementById("verticaltext").innerHTML = "VOTRE MAISON EST EXTRAORDINAIRE";
+  }
+
+  if (toprect3 >= 0 && toprect3 < hauteur)
+    {
+      document.getElementById("verticaltext").innerHTML = "PARLONS ENSEMBLE DE VOS PROJETS";
+    }
+
+  
+}
+
+document.addEventListener('scroll', update);
+update();
+
+
+
